9e4b90f112d1a1f7ec2ace06e08237caae739bef
Ningaraddi Raddi <124506771+ningaraddi-raddi@users.noreply.github.com>
2025-09-12 02:36:50 +0530

    Update arraySidebar.js

--- algorithm-visualizer-frontend/src/components/arrayOperations/arraySidebar.js
+++ algorithm-visualizer-frontend/src/components/arrayOperations/arraySidebar.js
@@ -12,6 +12,7 @@ import { ChevronRight, ChevronDown, BookOpen, GitBranch, Search, FlaskConical, C
 
 const topicsData = [
   { name: '1. Learn Arrays', route: '/array', icon: <BookOpen size={20} /> },
+  
   {
     name: '2. Basic Operations on Arrays',
     isCollapsible: true,
@@ -47,7 +48,78 @@ const topicsData = [
               },
             ],
           },
-        ];
+
+          {name: '3. classical patterns in Arrays',
+    isCollapsible: true,
+    icon: <GitBranch size={20} />,
+    subtopics: [
+     
+      {
+        name: '1. Two Pointers Pattern',
+        isCollapsible: true,
+        icon: <FlaskConical size={20} />,
+        subtopics: [
+          { name: '1.2 Sum problems', route: '/array/animate/twopointer/sum' },
+          { name: '2.2 Reversing arrays or strings.', route: '/array/animate/twopointer/reverse' },
+          
+          
+        ],
+            },
+
+            {
+        name: '2. Sliding Window Pattern',
+        isCollapsible: true,
+        icon: <FlaskConical size={20} />,
+        subtopics: [
+          { name: '2.1 Maximum/minimum sum of subarrays of size k.', route: '/array/animate/slidingwindow/max_sum_subarray' },
+          { name: '2. Longest Substring Without Repeating Characters', route: '/array/animate/slidingwindow/variable_size_window' },
+          
+          
+        ],
+            },
+
+
+             {
+        name: '3. Fast and Slow Pointers (Cycle Detection)',
+        isCollapsible: true,
+        icon: <FlaskConical size={20} />,
+        subtopics: [
+         { name: '3.1 Removing duplicates from sorted arrays.', route: '/array/animate/fast&slow/duplicates' },
+            { name: '3.2 Circular Array Loop .', route: '/array/animate/fast&slow/circulararray' },
+          
+          
+        ],
+            },
+
+             {
+        name: '4.Merge Intervals / Sorting Patterns',
+        isCollapsible: true,
+        icon: <FlaskConical size={20} />,
+        subtopics: [
+          { name: '4.1 Merge overlapping intervals.', route: '/array/animate/insertion/beginning' },
+          { name: '4.2 Meeting room scheduling problems.', route: '/array/animate/insertion/end' },
+          
+          
+        ],
+            },
+
+
+             {
+        name: '5. Cyclic / Rotational Patterns',
+        isCollapsible: true,
+        icon: <FlaskConical size={20} />,
+        subtopics: [
+          { name: '5.1 Rotate array by k steps.', route: '/array/animate/cyclic/rotate_array_by_ksteps' },
+          { name: '5.2 Maximum sum circular subarray (Kadane’s extension).', route: '/array/animate/insertion/end' },
+          
+          
+        ],
+            }
+
+
+          ]
+        }
+      ];
           
 
 
