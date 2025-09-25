effb0d19b05143fc42a48b9b0aaea362a912c27f
Ningaraddi Raddi <124506771+ningaraddi-raddi@users.noreply.github.com>
2025-09-25 16:04:12 +0530

    Update authService.js

--- algorithm-visualizer-frontend/src/services/authService.js
+++ algorithm-visualizer-frontend/src/services/authService.js
@@ -1,30 +1,30 @@
-// import axios from 'axios';
-// const API ='https://algomate-p45p.onrender.com';
-
-// export const registerUser = (payload) => axios.post(`${API}/register`, payload).then(r => r.data);
-// export const loginUser = (payload) => axios.post(`${API}/login`, payload).then(r => r.data);
-// export const getMe = (token) => axios.get(`${API}/me`, { headers: { Authorization: `Bearer ${token}` } }).then(r => r.data);
-
-// // helper
-// export const saveToken = (token) => localStorage.setItem('token', token);
-// export const getToken = () => localStorage.getItem('token');
-// export const removeToken = () => localStorage.removeItem('token');
-
-
-
-
 import axios from 'axios';
+const API ='https://algomate-p45p.onrender.com';
 
-const API = 'https://algomate-p45p.onrender.com/auth'; // include /auth
+export const registerUser = (payload) => axios.post(`${API}/register`, payload).then(r => r.data);
+export const loginUser = (payload) => axios.post(`${API}/login`, payload).then(r => r.data);
+export const getMe = (token) => axios.get(`${API}/me`, { headers: { Authorization: `Bearer ${token}` } }).then(r => r.data);
 
-export const registerUser = (payload) =>
-  axios.post(`${API}/register`, payload, { withCredentials: true }).then(r => r.data);
+// helper
+export const saveToken = (token) => localStorage.setItem('token', token);
+export const getToken = () => localStorage.getItem('token');
+export const removeToken = () => localStorage.removeItem('token');
 
-export const loginUser = (payload) =>
-  axios.post(`${API}/login`, payload, { withCredentials: true }).then(r => r.data);
 
-export const getMe = () =>
-  axios.get(`${API}/me`, { withCredentials: true }).then(r => r.data);
+
+
+// import axios from 'axios';
+
+// const API = 'https://algomate-p45p.onrender.com/auth'; // include /auth
+
+// export const registerUser = (payload) =>
+//   axios.post(`${API}/register`, payload, { withCredentials: true }).then(r => r.data);
+
+// export const loginUser = (payload) =>
+//   axios.post(`${API}/login`, payload, { withCredentials: true }).then(r => r.data);
+
+// export const getMe = () =>
+//   axios.get(`${API}/me`, { withCredentials: true }).then(r => r.data);
 
 
 
