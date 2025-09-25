6ce2acf84c7ccd9e22cfe3f6d3fa65d77c04ec74
Ningaraddi Raddi <124506771+ningaraddi-raddi@users.noreply.github.com>
2025-09-25 15:09:56 +0530

    Update Register.js

--- algorithm-visualizer-frontend/src/components/Register.js
+++ algorithm-visualizer-frontend/src/components/Register.js
@@ -8,7 +8,7 @@
 
 import React, { useState } from 'react';
 import { useNavigate, Link } from 'react-router-dom';
-import { registerUser, saveToken } from '../services/authService';
+import { registerUser} from '../services/authService';
 
 export default function Register() {
   const [name, setName] = useState('');
